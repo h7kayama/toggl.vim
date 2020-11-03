d95190350b6328ae7d0ab87109bc1a04cd5ddf4e
h7kayama <0hideki0@gmail.com>
2020-11-03 22:02:43 +0900

    add syntax highlight

--- rplugin/python3/denite/source/project.py
+++ rplugin/python3/denite/source/project.py
@@ -2,6 +2,10 @@
 
 from .base import Base
 
+BUFFER_HIGHLIGHT_SYNTAX = [
+    {'name': 'Client', 'link': 'Comment',     're': r'(.\+)'}
+]
+
 class Source(Base):
 
     def __init__(self, vim):
@@ -15,13 +19,22 @@ class Source(Base):
         client_names = map(lambda c: c['name'], clients)
         context['__clients'] = dict(zip(client_ids, client_names))
 
+    def highlight(self) -> None:
+        for syn in BUFFER_HIGHLIGHT_SYNTAX:
+            self.vim.command(
+                'syntax match {0}_{1} /{2}/ contained containedin={0}'.format(
+                    self.syntax_name, syn['name'], syn['re']))
+            self.vim.command(
+                'highlight default link {}_{} {}'.format(
+                    self.syntax_name, syn['name'], syn['link']))
+
     def gather_candidates(self, context):
         projects = self.vim.call('toggl#projects')
         return [self._convert(project, context) for project in projects]
 
     def _convert(self, info, context):
-        client_name = context['__clients'][info.get('cid')] if info.get('cid') else ''
-        abbr = '%s %s' % (info['name'], client_name)
+        client_name = '({})'.format(context['__clients'][info.get('cid')]) if info.get('cid') else ''
+        abbr = '{} {}'.format(info['name'], client_name)
         return {
                 'word': info['name'],
                 'abbr': abbr,
